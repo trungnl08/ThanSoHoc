5c3df20372e4ee9f9711e88f50f6f102dc1e8082
trungnl08 <trung80.uet@gmail.com>
2021-01-13 17:47:33 +0700

    bum

--- ThanSoHoc/Component/Home.js
+++ ThanSoHoc/Component/Home.js
@@ -7,27 +7,39 @@ import {
   TextInput,
   Button,
 } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+
 import DatePicker from 'react-native-date-picker';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const HomeScreen = ({navigation}) => {
   const [date, setDate] = useState(new Date());
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
 
   return (
     <View style={{backgroundColor: '#fff', flex: 1}}>
       <View style={{alignItems: 'center', padding: 10}}>
-        <View>
+        <View style={{paddingBottom: 50}}>
           <Text>Full name :</Text>
 
           <TextInput placeholder="....."></TextInput>
         </View>
-        <DatePicker mode="date" date={date} onDateChange={setDate} />
-        {console.log(date)}
-        <Button
+        <Text>Ngay sinh</Text>
+        <DatePicker
+          mode="date"
+          date={date}
+          onDateChange={setDate}
+          androidVariant="nativeAndroid"
+          fadeToColor="#fff"
+        />
+       
+      </View>
+      <Text>Gioi tinh</Text>
+        
+           <Button
           title="Go"
           onPress={() => navigation.navigate('Carousel')}></Button>
-      </View>
     </View>
   );
 };
