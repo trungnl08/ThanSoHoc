98b5e8234d06072fab3f33d323306db9e42fa7ca
duckSern1108 <nguyendacson2001@gmail.com>
2021-01-13 20:40:37 +0700

    update số nhân cách , khát tâm, đường đời số

--- thansohoc.js
+++ thansohoc.js
@@ -7,7 +7,7 @@ function soNhanCach(name) {
     const input = name.toLowerCase()
     let num = 0
     for (let i = 0 ; i < input.length ; i++) {
-        if (isCharacter(input[i])) {
+        if (isCharacter(input[i]) && !isNguyenAm(input[i])) {
             for (let j = 0 ; j < alb.length ; j++) {
                 if (input[i] === alb[j]) {
                     num += (j % 9 + 1)
@@ -15,7 +15,30 @@ function soNhanCach(name) {
             }
         }
     }
-    num
+    num = laySoDon(num)
+    return num
+}
+const isNumber = char => char >= '0' && char <= '9' 
+function duongDoiSo(date) {
+    let num = 0
+    for (let i = 0 ; i < date.length ; i++) {
+        if (isNumber(date[i])) num += parseInt(date[i])
+    }
+    return laySoDon(num)
+}
+const isNguyenAm = char => char === 'u' || char === 'e' || char === 'o' || char === 'a' || char === 'i'
+function soKhatTam(name) {
+    const input = name.toLowerCase()
+    let num = 0
+    for (let i = 0 ; i < input.length ; i++) {
+        if (isNguyenAm(name[i])) {
+            for (let j = 0 ; j < alb.length ; j++) {
+                if (input[i] === alb[j]) {
+                    num += (j % 9 + 1)
+                }
+            }
+        }
+    }
     num = laySoDon(num)
     return num
 }
@@ -33,4 +56,9 @@ function laySoDon(num) {
     }
     return num
 }
-console.log(soNhanCach("le ngoc trung"))
+const today = new Date("8/11/2001")
+console.log(today.getMonth());
+const todayS = [today.getDate(),today.getMonth() + 1,today.getFullYear()].toString()
+console.log(soNhanCach("nguyen dac son"))
+console.log(soKhatTam("nguyen dac son"))
+console.log(duongDoiSo(todayS))
