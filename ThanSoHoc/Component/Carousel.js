7e53c23a7acb745ba6e7d38042d18080f4f7c169
trungnl08 <trung80.uet@gmail.com>
2021-01-14 10:56:46 +0700

    done test print data

--- ThanSoHoc/Component/Carousel.js
+++ ThanSoHoc/Component/Carousel.js
@@ -8,15 +8,38 @@ import {
   Button,
 } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
-import {laySoDon, soKhatTam} from '../thansohoc'
+import { laySoDon, soKhatTam } from '../thansohoc'
+import { dataDuongDoi, dataKhatTam } from './data'
+
+
 const CarouselScreen = ({route, navigation}) => {
-  const {P1,P2} = route.params;
-  let duongDoiNgay = laySoDon(parseInt(P1));
+  const { P1, P2 } = route.params;
+  
+  let setData = new Array()
+  const check = (e,ind) => {
+    for (let i = 0; i < e.length; i++){
+      if (e[i].num === ind) {
+        setData.push(e[i]);
+      }
+    }
+  }
+  check(dataDuongDoi, laySoDon(parseInt(P1)))
+  check(dataKhatTam, soKhatTam(P2));
+
+  
 
   return (
     <View>
-          <Text>This is vmvmvmvmv {duongDoiNgay} </Text>
-          <Text>{soKhatTam(P2)}</Text>
+          <Text>duong theo ngay sinh {laySoDon(P1)} </Text>
+      <Text> duong khat tam {soKhatTam(P2)}</Text>
+      {setData.map((item, index) => {
+        return (
+          <View  key={index}>
+            <Text>{item.title}</Text>
+            <Text>{item.content}</Text>
+          </View>
+        )
+      })}
     </View>
   );
 };
