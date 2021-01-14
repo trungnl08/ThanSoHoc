0bc259c84d9b0b1645f7be775d053b0a371fcb1f
trungnl08 <trung80.uet@gmail.com>
2021-01-14 10:56:46 +0700

    done test print data

---
+++ ThanSoHoc/Component/data.js
@@ -0,0 +1,36 @@
+const dataDuongDoi = [
+    {
+        title:'Đường đời số',
+        num: 1,
+        content:'Một nhà lãnh đạo bẩm sinh; có tài quản lý; đừng biến mình trở thành kẻ theo đuôi người khác; làm chủ cuộc sống của mình.'
+    },
+    {
+        title:'Đường đời số',
+        num: 2,
+        content:'Sự phát triển mạnh mẽ ở mặt tinh thần; có khuynh hướng tâm linh cao; hãy tâm linh tích cực, đừng trở nên cực đoan.'
+    },
+    {
+        title:'Đường đời số',
+        num: 3,
+        content:'Sự sáng tạo; khả năng giao tiếp tốt; người của cộng đồng và có khuynh hướng nghệ thuật; đừng để cảm xúc bị phai mờ, hãy khơi dậy sự nhiệt huyết bên trong để kích thích sáng tạo.'
+    }
+]
+
+const dataKhatTam= [
+    {
+        title:'Con số khát tâm',
+        num: 1,
+        content:'Bạn là người mang tính cạnh tranh cao và cảm thấy thỏa mãn khi giành chiến thắng trong tất cả những cuộc thi hoặc trong việc làm nào đó'
+    },
+    {
+        title:'Con số khát tâm',
+        num: 2,
+        content:'Bạn cần sự cân bằng, hài hòa , an yên, và tình yêu để luôn cảm thấy đầy đủ, mãn nguyện.'
+    },
+    {
+        title:'Con số khát tâm',
+        num: 3,
+        content:'Bạn thích tương tác với xã hội, được gắn kết với cộng đồng và thể hiện khả năng của mình khiến bạn hạnh phúc'
+    }
+]
+export {dataDuongDoi, dataKhatTam}
