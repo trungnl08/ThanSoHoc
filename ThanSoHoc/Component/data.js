a5ab3eaef6ad5f1c9abe2e5306409c6f2c02642f
duckSern1108 <nguyendacson2001@gmail.com>
2021-01-15 14:36:56 +0700

    Update data.js

--- ThanSoHoc/Component/data.js
+++ ThanSoHoc/Component/data.js
@@ -13,7 +13,37 @@ const dataDuongDoi = [
         title:'Đường đời số',
         num: 3,
         content:'Sự sáng tạo; khả năng giao tiếp tốt; người của cộng đồng và có khuynh hướng nghệ thuật; đừng để cảm xúc bị phai mờ, hãy khơi dậy sự nhiệt huyết bên trong để kích thích sáng tạo.'
-    }
+    },
+     {
+        title:'Đường đời số',
+        num: 4,
+        content:'Sự thực tế; người thích hiện thực hóa một sự việc nào đó; đừng để chìm vào trong những thói quen, nên biến đổi liên tục.'
+    },
+     {
+        title:'Đường đời số',
+        num: 5,
+        content:'Sự tiến bộ; người muốn đem đến những điều tốt đẹp nhất cho thế giới; Đừng để bản thân bị mất phương hướng, tập trung vào những gì cần thiết.'
+    },
+     {
+        title:'Đường đời số',
+        num: 6,
+        content:'Sự chăm sóc; người nuôi dưỡng tâm hồn và vật chất; hãy chọn lối sống có trách nhiệm và biết nhiệm vụ của bản thân là gì.'
+    },
+     {
+        title:'Đường đời số',
+        num: 7,
+        content:'Sự vượt bậc trong tư duy; người có những suy nghĩ táo bạo, tuyệt vời; hãy chắc chắn về việc bản thân vẫn giữ được phong độ.'
+    },
+     {
+        title:'Đường đời số',
+        num: 8,
+        content:'Tham vọng; ngưởi muốn đạt được thành tựu nào đó một cách quyết liệt; đừng để đánh mất các mối quan hệ tốt đẹp vì tham công tiếc việc hoặc bị ám ảnh bởi công việc.'
+    },
+     {
+        title:'Đường đời số',
+        num: 9,
+        content:'Một nhà triết học và nhân đạo; hãy sống một cuộc sống chất lượng với những công việc thật thực tế.'
+    },
 ]
 
 const dataKhatTam= [
@@ -31,6 +61,141 @@ const dataKhatTam= [
         title:'Con số khát tâm',
         num: 3,
         content:'Bạn thích tương tác với xã hội, được gắn kết với cộng đồng và thể hiện khả năng của mình khiến bạn hạnh phúc'
-    }
+    },
+    {
+        title:'Con số khát tâm',
+        num: 4,
+        content:'Bạn khao khát chứng minh những kiến thức của mình, được mọi người công nhận kiến thức và chuyên môn làm bạn thấy thỏa mãn.'
+    },
+    {
+        title:'Con số khát tâm',
+        num: 5,
+        content:'Bạn là một linh hồn tự do, không có gì quý hơn và đáng trân trọng hơn bằng sự tự do.'
+    },
+    {
+        title:'Con số khát tâm',
+        num: 6,
+        content:'Bạn là người có nhu cầu mạnh mẽ về những nổ lực nuôi dưỡng của bạn được ghi nhận và đánh giá cao.'
+    },
+    {
+        title:'Con số khát tâm',
+        num: 7,
+        content:'Bạn cảm thấy hài lòng khi mà có thời gian và một không gian vừa đủ để theo đuổi hành trình tâm linh của mình.'
+    },
+    {
+        title:'Con số khát tâm',
+        num: 8,
+        content:'Địa vị, tự do tài chính và thành công về vật chất, tiền bạc sẽ khiến cho bạn cảm thấy hạnh phúc.'
+    },
+    {
+        title:'Con số khát tâm',
+        num: 9,
+        content:'Được đấu tranh cho bản thân và đóng góp công sức cho nhân loại sẽ khiến bạn cảm thấy đông đầy.'
+    },
+  
 ]
-export {dataDuongDoi, dataKhatTam}
+const dataNhanCach= [
+    {
+        title:'Con số nhân cách',
+        num: 1,
+        content:'Bạn ước mơ được vươn lên tầm cao nhất và được công nhận về lòng dũng cảm và khả năng lãnh đạo.'
+    },
+    {
+        title:'Con số nhân cách',
+        num: 2,
+        content:'Bạn ước mơ được trở thành người giải hòa, đem lại sự cân bằng và hòa hợp đến cho mọi người.'
+    },
+    {
+        title:'Con số nhân cách',
+        num: 3,
+        content:'Bạn ước mơ được thể hiện hết khả năng sáng tạo và tài nghệ thuật của mình kèm theo lời tán dương về nó.'
+    },
+    {
+        title:'Con số nhân cách',
+        num: 4,
+        content:'Bạn ước mơ được hoàn thành kế hoạch của mình một cách trọn vẹn và dễ dàng đoán trước được điều đó.'
+    },
+    {
+        title:'Con số nhân cách',
+        num: 5,
+        content:'Bạn ước mơ được tự do là chính.'
+    },
+    {
+        title:'Con số nhân cách',
+        num: 6,
+        content:'Bạn ước mơ được chăm sóc, quan tâm đến gia đình của bạn.'
+    },
+    {
+        title:'Con số nhân cách',
+        num: 7,
+        content:'Bạn ước mơ được đắm chìm trong sự hăng say theo đuổi một kiến thức nào đó.'
+    },
+    {
+        title:'Con số nhân cách',
+        num: 8,
+        content:'Bạn ước mơ có được quyền lực và của cải vật chất.'
+    },
+    {
+        title:'Con số nhân cách',
+        num: 9,
+        content:'Bạn ước mơ được đóng góp công sức của mình cho toàn nhân laoi5 được tốt đẹp hơn.'
+    },
+    {
+        title:'Con số nhân cách',
+        num: 11,
+        content:'Bạn là một người duy tâm thực sự và có ước mơ mang lại ánh sáng cho thế giới.'
+    },
+    {
+        title:'Con số nhân cách',
+        num: 22,
+        content:'Bạn ước mơ đạt được một thành tựu nào đó thực sự hoành tráng.'
+    },
+]
+const dataDinhMenh= [
+    {
+        title:'Con số định mệnh',
+        num: 1,
+        content:'Định mệnh sắp đặt sẽ phát triển rất tốt trên con đường trở thành một nhà lãnh đạo tài ba.'
+    },
+    {
+        title:'Con số định mệnh',
+        num: 2,
+        content:'Định mệnh sắp đặt sẽ phát triển mạnh trên con đường học tập tâm linh hoặc thức tỉnh tâm linh.'
+    },
+    {
+        title:'Con số định mệnh',
+        num: 3,
+        content:'Định mệnh sắp đặt sẽ trở thành một người truyền cảm hứng cho người khác thông qua sự sáng tạo và năng lượng tích cực.'
+    },
+    {
+        title:'Con số định mệnh',
+        num: 4,
+        content:'Định mệnh sắp đặt sẽ phát triển như một “bậc thầy xây dựng” trên con đường đạt được ước mơ và những mục tiêu dài hạn.'
+    },
+    {
+        title:'Con số định mệnh',
+        num: 5,
+        content:'Định mệnh sắp đặt sẽ thúc đẩy được sự thay đổi thông qua hành động cụ thể mang tính cấp tiến.'
+    },
+    {
+        title:'Con số định mệnh',
+        num: 6,
+        content:'Định mệnh sắp đặt sẽ thành công trong việc giúp đỡ, đóng góp hỗ trợ các hoạt động cộng đồng, tình yêu và lòng trách nhiệm cao.'
+    },
+    {
+        title:'Con số định mệnh',
+        num: 7,
+        content:'Định mệnh sắp đặt sẽ thúc đẩy ranh giới của tri thức ngày càng rộng ra thông qua khả năng tư duy vượt bậc.'
+    },
+    {
+        title:'Con số định mệnh',
+        num: 8,
+        content:'Định mệnh sắp đặt sẽ leo được lên tới đỉnh cao của thế giới vật chất.'
+    },
+    {
+        title:'Con số định mệnh',
+        num: 9,
+        content:'Định mệnh sắp đặt phải trở thành một người anh hoặc chị hay đơn giản là người trưởng thành và sẽ nâng đỡ người khác.'
+    },
+   ]
+export {dataDuongDoi, dataKhatTam,dataNhanCach,dataDinhMenh}
