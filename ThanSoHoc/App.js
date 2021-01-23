e135e2cd2a8c6fd38a43f0aa722ea2d2b55ea2e6
trungnl08 <trung80.uet@gmail.com>
2021-01-23 10:41:00 +0700

    lam lai

--- ThanSoHoc/App.js
+++ ThanSoHoc/App.js
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import SplashScreen from 'react-native-splash-screen';
+
 import {
   StyleSheet,
   View,
@@ -12,19 +12,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './Component/Home'
 import CarouselScreen from './Component/Carousel'
-import StartScreen from './Component/Start'
 
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  // React.useEffect(() => {SplashScreen.hide()},[])
   return (
 
   <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" headerMode='none'>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name='Start' component={StartScreen} />
         <Stack.Screen name="Carousel" component={CarouselScreen} />
       </Stack.Navigator>
     </NavigationContainer>
