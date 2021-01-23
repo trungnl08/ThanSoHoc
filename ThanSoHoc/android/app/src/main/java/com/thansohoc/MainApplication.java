c9632068b67bc3df6fc57ae8a3b18ca50597b142
trungnl08 <trung80.uet@gmail.com>
2021-01-23 15:41:16 +0700

    ...

--- ThanSoHoc/android/app/src/main/java/com/thansohoc/MainApplication.java
+++ ThanSoHoc/android/app/src/main/java/com/thansohoc/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
