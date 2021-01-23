d9120633bb2ee7257c4aba74b7d81e48384c7819
trungnl08 <trung80.uet@gmail.com>
2021-01-23 10:07:28 +0700

    zzzzzzzzzzz

--- ThanSoHoc/App.js
+++ ThanSoHoc/App.js
@@ -12,17 +12,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './Component/Home'
 import CarouselScreen from './Component/Carousel'
+import StartScreen from './Component/Start'
 
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  React.useEffect(() => {SplashScreen.hide()},[])
+  // React.useEffect(() => {SplashScreen.hide()},[])
   return (
 
   <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" headerMode='none'>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name='Start' component={StartScreen} />
         <Stack.Screen name="Carousel" component={CarouselScreen} />
       </Stack.Navigator>
     </NavigationContainer>
