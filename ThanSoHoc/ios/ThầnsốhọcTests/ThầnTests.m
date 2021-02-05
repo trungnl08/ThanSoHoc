5484b82b58d570f698475cb9972d8d9ab985d388
trungnl08 <trung80.uet@gmail.com>
2021-02-05 22:34:42 +0700

    .

--- ThanSoHoc/ios/ThầnsốhọcTests/ThầnTests.m
+++ ThanSoHoc/ios/ThầnsốhọcTests/ThầnTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ThanSoHocTests : XCTestCase
+@interface ThầnTests : XCTestCase
 
 @end
 
-@implementation ThanSoHocTests
+@implementation ThầnTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
