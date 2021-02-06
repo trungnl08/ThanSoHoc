756c36bae9a40549ce0e50847996e37f153dbe13
trungnl08 <trung80.uet@gmail.com>
2021-02-06 14:02:24 +0700

    fix bug + 1.0 music

---
+++ ThanSoHoc/services.ts
@@ -0,0 +1,18 @@
+import TrackPlayer from 'react-native-track-player'
+export default async function services() {
+
+    TrackPlayer.addEventListener('remote-play', () => TrackPlayer.play());
+
+    TrackPlayer.addEventListener('remote-pause', () => TrackPlayer.pause());
+
+    TrackPlayer.addEventListener('remote-stop', () => TrackPlayer.destroy());
+    TrackPlayer.addEventListener('playback-track-changed', () => {});
+    TrackPlayer.addEventListener('playback-state', (state) => {
+        console.log('play state', state);
+        
+    });
+
+
+    // ...
+    
+};
